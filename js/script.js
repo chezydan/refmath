1b199ab36c7d0bbe5db926c07c347d126d2951b2
dan <chezydan@gmail.com>
2019-11-07 13:44:08 +0200

    improve style on tri area

--- js/script.js
+++ js/script.js
@@ -6,9 +6,14 @@ const b =document.getElementById("b");
 const C =document.getElementById("C");
 
 const sinC=document.getElementById("sinC");
+const Display=document.getElementById("display");
 Solve.addEventListener('click', () => {
-Result.textContent=  Math.sin(C.value * Math.PI / 180)*a.value*b.value*(1/2);
+Result.textContent=  (Math.sin(C.value * Math.PI / 180)*a.value*b.value*(1/2)).toFixed(3);
 
-sinC.value =Math.sin(C.value * Math.Pi /180);
+sinC.innerHTML ="sin(C)="+ ((Math.sin(C.value * Math.PI /180))).toFixed(3);
+
+});
+C.addEventListener('input', ()=>{
+Display.textContent="1/2" +" "+a.value+" "+b.value +" sin("+C.value+")";
 
 });
